cf8fd7abb6d7509fb7c8152b146be9ae141c9160
Sergey Klimov <sergey.v.klimov@gmail.com>
2021-10-22 03:53:02 -0700

    added basic cpp class and rider project

---
+++ Source/UnrealPDFExport/BasicCPPClass.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BasicCPPClass.h"
+
+BasicCPPClass::BasicCPPClass()
+{
+}
+
+BasicCPPClass::~BasicCPPClass()
+{
+}
---
+++ Source/UnrealPDFExport/BasicCPPClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class UNREALPDFEXPORT_API BasicCPPClass
+{
+public:
+	BasicCPPClass();
+	~BasicCPPClass();
+};
